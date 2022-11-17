c7f8cfb934c6e00a71f7e1b90c537484224cb7e4
Anna <anna.93.08@mail.ru>
2022-11-17 22:32:02 +0300

    Задача 1 - Определение дня недели по числу

---
+++ Task1.py
@@ -0,0 +1,18 @@
+print("Введите цифру от 1 до 7");
+day=int(input())
+if day==1:
+    print("Понедельник")
+elif day==2:
+    print("Вторник")
+elif day==3:
+    print("Среда")
+elif day==4:
+    print("Четверг")
+elif day==5:
+    print("Пятница")
+elif day==6:
+    print("Суббота")
+elif day==7:
+    print("Воскресенье")
+else:
+    print("Вы ввели неверное число")
