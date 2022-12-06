f3c3383da28694a6371ecbc80d291b665f6be9ba
Anna <anna.93.08@mail.ru>
2022-12-06 13:39:07 +0300

    Удаление из текста слов

--- Task16.py
+++ Task16.py
@@ -7,12 +7,3 @@ def del_words(txt):
 txt = input("Введите текст\n")
 print(f"Исходный текст: {txt}")
 print(f'Результат: {del_words(txt)}')
-
-"""data = open("text.txt", "w")
-data.write(input())
-data.close()
-
-data = open("text.txt", "r+")
-del_words(data)
-data.close()
-"""
